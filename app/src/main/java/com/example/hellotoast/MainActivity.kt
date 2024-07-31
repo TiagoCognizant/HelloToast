da1385c2ee91b2969a0759566c4a4568dd9cdb2d
Tiago Zheng <tiago.zheng23@gmail.com>
2024-07-31 10:13:46 +0100

    Button Count Up App

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.hellotoast
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun countUp(view: View) {
+        var count_Display : TextView = findViewById(R.id.show_count)
+        var data = count_Display.text.toString()
+        var intCount = data.toInt()
+        intCount++
+        data = intCount.toString()
+        count_Display.setText(data)
+
+    }
+}
